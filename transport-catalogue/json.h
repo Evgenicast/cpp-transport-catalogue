e7dea2afacc2d036074cccffca1b2f257a32f7f7
Evgeny <pronex@yandex.ru>
2023-02-18 19:42:17 +0300

    TransportCatalogue v2.0

--- transport-catalogue/json.h
+++ transport-catalogue/json.h
@@ -1,3 +1,5 @@
+#ifndef JSON_H
+#define JSON_H
 #pragma once
 #include <iostream>
 #include <map>
@@ -6,29 +8,29 @@
 
 namespace json
 {
-	class Node;
-	using Dict = std::map<std::string, Node>;
-	using Array = std::vector<Node>;
+    class Node;
+    using Dict = std::map<std::string, Node>;
+    using Array = std::vector<Node>;
 
-	class ParsingError
-		: public std::runtime_error
-	{
-	public:
+    class ParsingError
+        : public std::runtime_error
+    {
+    public:
 
-		using runtime_error::runtime_error;
-	};
+        using runtime_error::runtime_error;
+    };
 
-	class Node final : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string>
-	{
-	public:
+    class Node final : private std::variant<std::nullptr_t, Array, Dict, bool, int, double, std::string>
+    {
+    public:
 
-		using variant::variant;
-		using Value = variant;
+        using variant::variant;
+        using Value = variant;
 
         bool IsInt() const;
         int AsInt()  const;
         bool IsPureDouble() const;
-		bool IsDouble() const;
+        bool IsDouble() const;
         double AsDouble() const;
         bool IsBool() const;
         bool AsBool() const;
@@ -41,42 +43,44 @@ namespace json
         const Dict & AsDict() const;
         bool operator==(const Node& rhs) const;
         const Value & GetValue() const;
-	};
+    };
 
     inline bool operator!=(const Node& lhs, const Node& rhs)
     {
         return !(lhs == rhs);
     }
 
-	class Document
-	{
+    class Document
+    {
     private:
 
         Node root_;
 
-	public:
+    public:
 
-		explicit Document(Node root)
-			: root_(std::move(root)) {}
+        explicit Document(Node root)
+            : root_(std::move(root)) {}
 
-		const Node& GetRoot() const
-		{
+        const Node& GetRoot() const
+        {
             return root_;
-		}
-	};
+        }
+    };
 
     inline bool operator==(const Document & lhs, const Document & rhs)
-	{
-		return lhs.GetRoot() == rhs.GetRoot();
-	}
+    {
+        return lhs.GetRoot() == rhs.GetRoot();
+    }
 
     inline bool operator!=(const Document & lhs, const Document & rhs)
-	{
-		return !(lhs == rhs);
-	}
+    {
+        return !(lhs == rhs);
+    }
 
     Document Load(std::istream & Input);
 
     void Print(const Document & Doc, std::ostream & Output);
 
 } // namespace json
+
+#endif // JSON_H
